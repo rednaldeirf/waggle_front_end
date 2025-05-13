840add10835b2ae8f4b1c5cbb7bf97d4d19d0e57
rednaldeirf <jason.d.friedlander@gmail.com>
2025-05-13 13:35:57 -0400

    prelim components, pet list, preference

---
+++ src/services/pets.js
@@ -0,0 +1,19 @@
+import api from "./apiConfig";
+
+// Fetch all pets of a given type (dog/cat)
+export async function fetchPets(type) {
+  const resp = await api.get(`/pets/?species=${type}`);
+  return resp.data;
+}
+
+// Fetch all breeds for a type
+export async function fetchBreeds(type) {
+  const resp = await api.get(`/pets/breeds/?species=${type}`);
+  return resp.data;
+}
+
+// Fetch all shelters
+export async function fetchShelters() {
+  const resp = await api.get("/shelters/");
+  return resp.data;
+} 
